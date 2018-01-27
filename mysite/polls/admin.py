94ceb0e3f39191db56c63a543fe0c444a3dbf4f5
HalukaMB <mail@maier-borst.de>
2018-01-27 19:07:56 +0100

    finished

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,31 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+
+    #OR admin.StackedInline
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    #this is for the individual question page
+
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    #this is for the question page
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    #fields = ['pub_date', 'question_text']
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,33 +2,28 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render, Http404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
-
-
+from django.views import generic
 # Create your views here.
 from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    #context = {
-    #    'latest_question_list': latest_question_list,
-    #}
-    #return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    #try:
-#        question = Question.objects.get(pk=question_id)
-#    except Question.DoesNotExist:
-#        raise Http404("Question does not exist")
-#    return render(request, 'polls/detail.html', {'question': question})
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     #tries to get the question fitting to the request
@@ -56,7 +51,3 @@ def vote(request, question_id):
         #saved in urls.py and the question.id is sent as an argument
         #it will redirect to an url like "'/polls/3/results/'"
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
